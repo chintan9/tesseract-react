55a61ae870d269fe9c365f56cf60780a66e94017
Chintan Prajapati <chintan9@student.fdu.edu>
2020-11-24 00:04:23 +0000

    remove unused code

--- src/App.js
+++ src/App.js
@@ -2,49 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { createWorker } from 'tesseract.js';
 import './App.css';
 
-
-function App() {
-  const [date, setDate] = useState(null);
-   const worker = createWorker({
-    logger: m => console.log(m),
-  });
-  const doOCR = async () => {
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-    setOcr(text);
-  };
-  const [ocr, setOcr] = useState('Recognizing...');
-  useEffect(() => {
-    doOCR();
-  });
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <div className="App">
-      <p>{ocr}</p>
-    </div>
-      <br />
-      <h2>Website Loaded on:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
-}
-
-export default App;
-/*
-import React, { useEffect, useState } from 'react';
-import { createWorker } from 'tesseract.js';
-import './App.css';
-
 function App() {
   const worker = createWorker({
     logger: m => console.log(m),
@@ -53,7 +10,7 @@ function App() {
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    const { data: { text } } = await worker.recognize('hw.png');
     setOcr(text);
   };
   const [ocr, setOcr] = useState('Recognizing...');
@@ -68,5 +25,3 @@ function App() {
 }
 
 export default App;
-*/
-
