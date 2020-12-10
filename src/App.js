8bf21e5ad8a4d7284d4ee5da6e2575ea509f5cba
chintan9 <chintan9@users.noreply.github.com>
2020-12-10 22:42:29 +0000

    Prettified Code!

--- src/App.js
+++ src/App.js
@@ -1,66 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { createWorker } from 'tesseract.js';
-import { useFileUpload } from 'use-file-upload'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { createWorker } from "tesseract.js";
+import { useFileUpload } from "use-file-upload";
+import "./App.css";
 
 function App() {
-    const [workDone, setworkDone] = useState(0);
+  const [workDone, setworkDone] = useState(0);
 
-    const worker = createWorker({
-        logger: m => console.log(m),
-        logger: m => setworkDone(m.progress),
-    });
-    const [file, selectFile] = useFileUpload()
-    const [filepath, setfilepath] = useState(null);
-  
-    const doOCR = async () => {
-        await worker.load();
-        await worker.loadLanguage('eng');
-        await worker.initialize('eng');
-        if (filepath !== null) {
-            const fileurl = URL.createObjectURL(filepath)
-            const { data: { text } } = await worker.recognize(fileurl);
-            setOcr(text);
-        } else {
-            return console.log("add image first")
-        }
-    };
-    const [ocr, setOcr] = useState('Recognizing...');
-    useEffect(() => {
-        doOCR();
-    },[filepath]);
+  const worker = createWorker({
+    logger: (m) => console.log(m),
+    logger: (m) => setworkDone(m.progress),
+  });
+  const [file, selectFile] = useFileUpload();
+  const [filepath, setfilepath] = useState(null);
 
-    return (
-        <main>
-            <button
-                onClick={() => {
-                    // Single File Upload
-                    selectFile({}, ({ source, name, size, file }) => {
-                        // file - is the raw File Object
-                        setfilepath(file)
-                        console.log({ source, name, size, file })
-                    })
-                }}
-            >
-                Click to Upload
+  const doOCR = async () => {
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    if (filepath !== null) {
+      const fileurl = URL.createObjectURL(filepath);
+      const {
+        data: { text },
+      } = await worker.recognize(fileurl);
+      setOcr(text);
+    } else {
+      return console.log("add image first");
+    }
+  };
+  const [ocr, setOcr] = useState("Recognizing...");
+  useEffect(() => {
+    doOCR();
+  }, [filepath]);
+
+  return (
+    <main>
+      <button
+        onClick={() => {
+          // Single File Upload
+          selectFile({}, ({ source, name, size, file }) => {
+            // file - is the raw File Object
+            setfilepath(file);
+            console.log({ source, name, size, file });
+          });
+        }}
+      >
+        Click to Upload
       </button>
 
-            {file ? (
-                <div>
-                    <span> File Uploaded:  </span> <br />
-                    <span> Name: {file.name} </span>
-                    <span> Size: {file.size} </span>
-                </div>
-            ) : (
-                    <span>No file selected</span>
-                )}
-            <div className="App">
-            <p>Work done: {workDone * 100 }%</p>
-                <br/>
-                <p>{ocr}</p>
-            </div>
-        </main>
-    );
+      {file ? (
+        <div>
+          <span> File Uploaded: </span> <br />
+          <span> Name: {file.name} </span>
+          <span> Size: {file.size} </span>
+        </div>
+      ) : (
+        <span>No file selected</span>
+      )}
+      <div className="App">
+        <p>Work done: {workDone * 100}%</p>
+        <br />
+        <p>{ocr}</p>
+      </div>
+    </main>
+  );
 }
 
 export default App;
